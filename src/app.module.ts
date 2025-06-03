7eeee870f500b7f2e926be9e0d9c9ede3afe7678
dev-avk <dev.avk777@gmail.com>
2025-06-03 15:44:34 +0300

    chore: add dotenv package and update app module imports
    add auth.module and cats.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { CatsModule } from './cats/cats.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 //Модуль — это класс, аннотированный декоратором @Module()
+//@Module — это функция-декоратор, которая применяется к классу и добавляет ему метаданные, необходимые фреймворку для понимания, как устроен этот модуль.
 @Module({
-  imports: [CatsModule],
+  imports: [CatsModule, AuthModule, UserModule],
 })
 export class AppModule {}
+// модули позволяют разбивать приложение на независимые части(компоненты), которые можно легко подключать и отключать
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { config } from 'dotenv';
+config();
 
 async function bootstrap() {
+  console.log('PORT:', process.env.PORT);
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3003);
 }
 
 bootstrap().catch((error) => {
