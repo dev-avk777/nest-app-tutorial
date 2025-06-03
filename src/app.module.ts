6d46a8a85f221b4da8ef0d713abe2caab20ad461
dev-avk <dev.avk777@gmail.com>
2025-06-03 16:32:58 +0300

    refactor: update app module imports and remove unused cats module
    Added BookmarkModule to app module and removed CatsModule as it is no longer needed. Also, included @nestjs/mapped-types dependency in package.json and updated pnpm-lock.yaml accordingly.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,12 @@
 import { Module } from '@nestjs/common';
-import { CatsModule } from './cats/cats.module';
-import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { BookmarkModule } from './bookmark/bookmark.module';
 
 //Модуль — это класс, аннотированный декоратором @Module()
 //@Module — это функция-декоратор, которая применяется к классу и добавляет ему метаданные, необходимые фреймворку для понимания, как устроен этот модуль.
 @Module({
-  imports: [CatsModule, AuthModule, UserModule],
+  imports: [AuthModule, UserModule, BookmarkModule],
 })
 export class AppModule {}
 // модули позволяют разбивать приложение на независимые части(компоненты), которые можно легко подключать и отключать
